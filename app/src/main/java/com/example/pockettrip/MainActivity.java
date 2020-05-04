63bb5be522b5d26b5f2d4fb8613e35253497b3cf
kimdain111 <kimdain111@naver.com>
2020-05-04 21:20:11 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pockettrip/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.pockettrip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.diary_plus); //activity_main.xml을 화면에 표시하라
+    }
+}
