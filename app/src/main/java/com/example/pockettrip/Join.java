e037635f83d3512cabc2595798b7a434f686cf14
ttrme1037 <64892413+ttrme1037@users.noreply.github.com>
2020-05-11 17:42:41 +0900

    join 수정

--- app/src/main/java/com/example/pockettrip/Join.java
+++ app/src/main/java/com/example/pockettrip/Join.java
@@ -1,15 +1,11 @@
 package com.example.pockettrip;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -27,7 +23,7 @@ public class Join extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.join);
-        Intent intent = getIntent();
+        //Intent intent = getIntent();
 
         final FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference myRef = database.getReference("user");
@@ -57,13 +53,11 @@ public class Join extends Activity {
                         }
 
                     }
-
                     @Override
                     public void onCancelled(@NonNull DatabaseError databaseError) {
 
                     }
                 });
-
                 Intent myintent = new Intent(Join.this,MainActivity.class);
                 startActivity(myintent);
                 finish();
